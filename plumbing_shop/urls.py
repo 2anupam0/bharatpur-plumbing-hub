fb9d7d717dea93eb8d312ee6964b35f2ead3dee2
Anup Tiwari <tiwarianup2030@gmail.com>
2026-07-16 22:20:42 +0545

    Fix media file serving in production - images now display on site

--- plumbing_shop/urls.py
+++ plumbing_shop/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
+def serve_media(request, path):
+    return serve(request, path, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path("admin/", include("shop.admin_panel_urls")),
@@ -11,3 +15,6 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    re_media = r'^media/(?P<path>.*)$'
+    urlpatterns += [re_path(re_media, serve_media)]
